087075f68a4a77b814e7ffc0bbebf51f08302d33
Jiaww <jiaww@seas.upenn.edu>
2018-02-05 21:27:15 -0500

    Finished

--- PCSamples/IntroGraphics/SnowMan/Utilities.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "Utilities.h"
-std::vector<uint16_t> reverseIndices(std::vector<uint16_t> indices) {
-	std::vector<uint16_t> reversed;
-	for (int i = indices.size() - 1; i >= 0; i--)
-		reversed.push_back(indices[i]);
-	return reversed;
-}
--- PCSamples/IntroGraphics/SnowMan/Utilities.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <vector>
-
-std::vector<uint16_t> reverseIndices(std::vector<uint16_t> indices);
